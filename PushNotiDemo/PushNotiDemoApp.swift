f7728053a6ec3831f4173e5ed5d291d6cdc260b7
Terry Kuo <terrykuo501@gmail.com>
2022-04-14 16:42:28 +0800

    Initial Commit

---
+++ PushNotiDemo/PushNotiDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotiDemoApp.swift
+//  PushNotiDemo
+//
+//  Created by Terry Kuo on 2022/4/14.
+//
+
+import SwiftUI
+
+@main
+struct PushNotiDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
